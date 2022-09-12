150e1720581c543c1e438a681c8a18f2d2f2ef5e
João Paulo Mesquita <jp-mesquita@live.com>
2022-09-12 08:24:09 -0300

    first commit of mobile - find your duo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+function Button(props: ButtonProps){
+  return(
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <Button title='Send 1'/>
+      <Button title='Send 2'/>
+      <Button title='Send 3'/>
+      <Button title='Hello World'/>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+interface ButtonProps{
+  title: string
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 16,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
