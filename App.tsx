59723167fae10fe70f60609d5ca547867b77b64a
João Paulo Mesquita <jp-mesquita@live.com>
2022-09-13 13:23:45 -0300

    finishing the static landing page, mobile version

--- App.tsx
+++ App.tsx
@@ -1,44 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from 'react-native'
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
 
-
-function Button(props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { Home } from './src/screens/Home'
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
 
 
 export default function App() {
+
+  const [ fontsLoaded ] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
+
   return (
-    <View style={styles.container}>
+  <Background>
+    <StatusBar 
+      barStyle='light-content'
+      backgroundColor='transaparent'
+      translucent
+    />
 
-      <Button title='Send 1'/>
-      <Button title='Send 2'/>
-      <Button title='Send 3'/>
-      <Button title='Hello World'/>
-
-      <StatusBar style="auto" />
-    </View>
+    { fontsLoaded ? <Home /> : <Loading />}
+  </Background>
   );
 }
 
-interface ButtonProps{
-  title: string
-}
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    gap: 16,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-});
