03edc029e393f09aa4dccd4a47c639b7562c1af7
João Paulo Mesquita <jp-mesquita@live.com>
2022-09-15 15:02:31 -0300

    adding game screen, and conecting the api

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -6,9 +6,11 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
   id: string,
-  name: string,
-  ads: string,
-  cover: ImageSourcePropType
+  title: string,
+  bannerUrl: string,
+  _count:{
+    ads: number,
+  }
 }
 
 interface Props extends TouchableOpacityProps {
@@ -17,20 +19,20 @@ interface Props extends TouchableOpacityProps {
 
 export function GameCard( { data, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground 
         style={styles.cover}
-        source={data.cover}
+        source={{ uri: data.bannerUrl}}
       >
         <LinearGradient
           colors={THEME.COLORS.FOOTER}
           style={styles.footer}
           >
           <Text style={styles.name}>
-            {data.name}
+            {data.title}
           </Text>
           <Text style={styles.ads}>
-            {data.ads} anúncios
+            {data._count.ads} anúncios
           </Text>
         </LinearGradient>
       </ImageBackground>
